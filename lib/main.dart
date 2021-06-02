f744b5c0fad30dc1865a5c6e54d7c710002517d9
aeladrin <andriy.krupych@gmail.com>
2021-06-02 21:31:02 +0300

    playing sounds

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,60 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: TextButton(
-              child: Text("Play"),
-              onPressed: () {
-                final player = AudioCache();
-                player.play("note1.wav");
-              },
+            child: Column(
+              children: [
+                FlatButton(
+                  color: Colors.red,
+                  onPressed: () {
+                    playNote(1);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.orange,
+                  onPressed: () {
+                    playNote(2);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.yellow,
+                  onPressed: () {
+                    playNote(3);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.green,
+                  onPressed: () {
+                    playNote(4);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.teal,
+                  onPressed: () {
+                    playNote(5);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.blue,
+                  onPressed: () {
+                    playNote(6);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.purple,
+                  onPressed: () {
+                    playNote(7);
+                  },
+                ),
+              ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  void playNote(int note) {
+    final player = AudioCache();
+    player.play("note$note.wav");
   }
 }
